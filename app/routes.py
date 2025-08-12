d1dacb9f8fdfa1cedeb89f5e6426168f5d5b55e2
Kauã Angelo <144168394+KauaWn@users.noreply.github.com>
2025-08-12 11:46:01 -0300

    novo

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,21 @@ def terms():
 @app.route("/error404")
 def error404():
     return render_template("index.html")
+
+
+@app.route("/author_single")
+def author_single():
+    author = {
+        "nome": "asdsad",
+        "foto": "https://gravatar.com/avatar/27205e5c51cb03f862138b22bcb5dc20f94a342e744ff6df1b8dc8af3c865109"
+    }
+    return render_template("author_single.html", author_single = author)
+
+
+@app.route("/author_single2")
+def author_single2():
+    author = {
+        "nome": "afshgadfhfdh",
+        "foto": "https://www.gravatar.com/avatar/df5fe0c7d20b494dd2c68e0d8ef9bbf2?s=320&pg&d=identicon"
+    }
+    return render_template("author_single.html", author_single = author)
